454142872c8af1454d16536138cb15121683fc6d
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-21 14:54:28 +0600

    header and footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div>
-        EShop
+        <Header/>
+      
+        <main className="py-3">
+          <Container>
+            <h1>Welcome EShop</h1>
+          </Container>
+        </main>
+        
+        <Footer/>
     </div>
   );
 }
