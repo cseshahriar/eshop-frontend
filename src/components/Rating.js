c145e2309c6a6db61df1e676879eec1c109bfded
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-21 17:23:31 +0600

    rating component

---
+++ src/components/Rating.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+function Rating({value, text, color}) {
+    console.log(value);
+  return (
+    <div className="rating">
+        <span>
+           <i 
+                style={{color}} 
+                className={
+                    value >= 1 
+                        ? "fa fa-star" 
+                        : value >= 0.5 
+                            ? "fa fa-star-half-alt"
+                            : "far fa-star"
+                }
+            ></i>
+        </span>
+
+        <span>
+           <i 
+                style={{color}} 
+                className={
+                    value >= 2 
+                        ? "fa fa-star" 
+                        : value >= 1.5 
+                            ? "fa fa-star-half-alt"
+                            : "far fa-star"
+                }
+            ></i>
+        </span>
+
+
+        <span>
+           <i 
+                style={{color}} 
+                className={
+                    value >= 3 
+                        ? "fa fa-star" 
+                        : value >= 2.5 
+                            ? "fa fa-star-half-alt"
+                            : "far fa-star"
+                }
+            ></i>
+        </span>
+
+
+        <span>
+           <i 
+                style={{color}} 
+                className={
+                    value >= 4 
+                        ? "fa fa-star" 
+                        : value >= 3.5 
+                            ? "fa fa-star-half-alt"
+                            : "far fa-star"
+                }
+            ></i>
+        </span>
+
+        <span>
+           <i 
+                style={{color}} 
+                className={
+                    value >= 5 
+                        ? "fa fa-star" 
+                        : value >= 4.5 
+                            ? "fa fa-star-half-alt"
+                            : "far fa-star"
+                }
+            ></i>
+        </span>
+
+    </div>
+  )
+}
+
+export default Rating;
