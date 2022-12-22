86a581edca4c877c56278747875be121d5ef0e8e
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-22 08:32:23 +0600

    HomeScreen api cal fix

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -9,8 +9,9 @@ function HomeScreen() {
 
     useEffect(() => {
         async function fetchProducts() {
-            const {data} = await axios.get("http://127.0.0.1:8000/api/products/");
-            setProducts(data.products);
+            await axios.get("http://127.0.0.1:8000/api/products/")
+                .then(response => response.data)
+                .then(response => setProducts(response.data));
         }
         fetchProducts();
     }, []); // when component load
