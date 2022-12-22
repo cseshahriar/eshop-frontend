b5e35b14b733c54ff436b386ea6aa257d59cc714
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-22 09:23:02 +0600

    redux store created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
+// react imports
 import React from 'react';
+
+// third party imports
 import ReactDOM from 'react-dom/client';
 
+// import store
+import {Provider} from "react-redux";
+import store from "./store";
+
+// import css
 import './index.css';
 import './bootstrap.min.css';
 
+// import components
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// create root element and render
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
       <App />
-  </React.StrictMode>
+  </Provider>
 );
  
 reportWebVitals();
