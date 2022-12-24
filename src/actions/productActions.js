57cb64b0ca66b7c96a24fc2cbbadd44fc8dd6a29
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-24 08:04:04 +0600

    product action fixed

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -9,7 +9,8 @@ export const listProducts = (keyword = '') => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST })
 
-        const { data } = await axios.get(`/api/products${keyword}`)
+        const { data } = await axios.get(`http://127.0.0.1:8000/api/products`)
+            .then(response => response.data)
 
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
