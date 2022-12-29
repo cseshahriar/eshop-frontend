a3f7a9064ddd933c1f0385d24d0e3acd21b964e5
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-29 09:28:24 +0600

    cart screen

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,21 +1,21 @@
 import axios from "axios";
-import {CART_ADD_ITEM, CART_REMOVE_ITEM} from "../constants/cartConstants";
-import {PRODUCT_DETAILS_REQUEST} from "../constants/productConstants";
+import { CART_ADD_ITEM} from "../constants/cartConstants";
 
-export const addToCart = (id, qty) => async (dispatch, getState) =>{
+export const addToCart = (id, qty) => async (dispatch, getState) => {
     const { data } = await axios.get(`http://127.0.0.1:8000/api/products/${id}`)
         .then(response => response);
 
     dispatch({
         type: CART_ADD_ITEM,
         payload: {
-            product:data._id,
-            name:data.name,
-            image_url: data.image_url,
+            product: data._id,
+            name: data.name,
+            img_url: data.img_url,
             price: data.price,
             countInStock: data.countInStock,
             qty
         }
     });
-    localStorage.setItem('cartItems', JSON.stringify(getState.cart.cartItems));
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 }
--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,13 +1,108 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link, useParams, useLocation} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {Row, Col, ListGroup, Image, Form, Button, Card} from "react-bootstrap";
 
-class CartScreen extends Component {
-    render() {
-        return (
-            <div>
-                Cart
-            </div>
-        );
+import Message from "../components/Message";
+import {addToCart} from "../actions/cartActions";
+
+function CartScreen() {
+    const params = useParams();
+    const location = useLocation()
+    const dispatch = useDispatch();
+
+    const productId = params.id
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    // get data from state
+    const cart = useSelector(state => state.cart);
+    const { cartItems } = cart
+    console.log(cartItems);
+
+    useEffect(() => {
+        if(productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty]);
+
+    const removeFromCartHandler = (id) => {
+
     }
+
+    return (
+        <Row>
+            <Col md={8}>
+                <h1>Shopping Cart</h1>
+                {
+                    cartItems && cartItems.length === 0 ? (
+                        <Message variant='info'>Your cart is empty <Link to="/">Go Back</Link></Message>
+                    ) : (
+                        <ListGroup variant='flush'>
+                            {
+                                cartItems.map(item => (
+                                    <ListGroup.Item key={item.product}>
+                                        <Row>
+                                            <Col md={2}>
+                                                <Image src={item.img_url} alt={item.name} fluid rounded />
+                                            </Col>
+
+                                             <Col md={3}>
+                                                <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                            </Col>
+
+                                            <Col md={2}>
+                                                ${item.price}
+                                            </Col>
+
+                                            <Col md={3}>
+                                                <Form.Select aria-label="Default select example"
+                                                   value={item.qty}
+                                                   onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
+                                                >
+                                                    {
+                                                        [...Array(item.countInStock).keys()].map((el) => (
+                                                            <option value={el + 1 } key={ el + 1 }>{ el + 1}</option>
+                                                        ))
+                                                    }
+                                                </Form.Select>
+                                            </Col>
+
+                                            <Col md={1}>
+                                                <Button
+                                                    type='button'
+                                                    variant='light'
+                                                    onClick={() => removeFromCartHandler(item.product)}
+                                                >
+                                                    <i className='fas fa-trash'></i>
+                                                </Button>
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                ))
+                            }
+                        </ListGroup>
+                    )
+                }
+            </Col>
+
+            <Col md={4}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>Subtotal ({
+                                cartItems.reduce((acc, item) => acc + item.qty, 0)
+                            }) Items</h2>
+
+                            $ {
+                                cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)
+                            }
+
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
+        </Row>
+    );
 }
 
 export default CartScreen;
