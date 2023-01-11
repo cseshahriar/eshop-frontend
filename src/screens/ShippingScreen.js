09f8f450fd12f0828d6a583b3115e7a897d40695
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-01-11 19:59:38 +0600

    checkout status

--- src/screens/ShippingScreen.js
+++ src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import {useNavigate} from 'react-router-dom';
 import {useDispatch, useSelector} from "react-redux";
 import FormContainer from '../components/FormContainer'
 import { saveShippingAddress } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingScreen = () => {
     const dispatch = useDispatch();
@@ -27,6 +28,8 @@ const ShippingScreen = () => {
 
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2/>
+
             <h1 className="text-center">Shipping</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='address'>
