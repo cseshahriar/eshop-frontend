298fc33ef9bf2c682c337dec5e2e7a4298f95f88
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-01-24 20:20:23 +0600

    product image upload

--- src/screens/ProductEditScreen.js
+++ src/screens/ProductEditScreen.js
@@ -8,7 +8,7 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import FormContainer from '../components/FormContainer'
 import { listProductDetails, productUpdateAction } from "../actions/productActions";
-import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
+import { PRODUCT_UPDATE_RESET, PRODUCT_DELETE_REQUEST } from "../constants/productConstants";
 import { BASE_API_URL } from "../constants/baseConstants";
 
 function ProductEditScreen() {
@@ -26,6 +26,7 @@ function ProductEditScreen() {
     const [category, setCategory] = useState('');
     const [countInStock, setCountInStock] = useState(0);
     const [description, setDescription] = useState('');
+    const [uploading, setUploading] = useState(false);
 
     const productDetails = useSelector(state => state.productDetails);
     const {error, loading, product} = productDetails;
@@ -74,6 +75,32 @@ function ProductEditScreen() {
         )
     }
 
+
+    const uploadFileHandler = async (e) => {
+        // get file form fields
+        const file = e.target.files[0];
+        setImage(file);
+        const formData = new FormData();
+        formData.append('image', file);
+        formData.append('product_id', id);
+        setUploading(true);
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+            const { data } = await axios.post(
+                `${BASE_API_URL}products/upload/`, formData, config
+            );
+            setImage(data);
+            setUploading(false);
+        } catch (error) {
+            setUploading(false);
+        }
+    }
+
     return (
         <div>
             <Link to="/admin/productlist">Go Back</Link>
@@ -110,16 +137,16 @@ function ProductEditScreen() {
                                     </Form.Group>
 
                                     <Form.Group controlId='image'>
+                                        <img src={product.img_url} alt={product.name} className="img-thumbnail mt-2 mb-1" id="img-show" />
                                         <Form.Label>Image</Form.Label>
                                         <Form.Control
-                                            type='text'
-                                            placeholder='Enter image'
-                                            value={image}
-                                            onChange={(e) => setImage(e.target.value)}
-                                        >
-                                        </Form.Control>
-                                    </Form.Group>
+                                            type="file"
+                                            onChange={uploadFileHandler}
+                                            label='Choose File'
+                                        />
+                                        {uploading && <Loader />}
 
+                                    </Form.Group>
 
                                     <Form.Group controlId='brand'>
                                         <Form.Label>Brand</Form.Label>
